c361ec35c15bff31e864dcb4eb0f4e0a5885eabe
Matt Svoboda <matt.svoboda@gmail.com>
2022-10-09 19:27:58 -0700

    Improve comments

--- iina/EditableTableViewDelegate.swift
+++ iina/EditableTableViewDelegate.swift
@@ -13,9 +13,14 @@ import Foundation
 protocol EditableTableViewDelegate {
   func editDidEndWithNewText(newValue: String, row rowIndex: Int, column columnIndex: Int) -> Bool
 
-  // If true is returned, a row editor will be displayed for editing cell text
+  // The user double-clicked on the cell with the given row & coumn indexes.
+  // If true is returned, an in-line editor will be opened for editing the cell text.
+  // If false is returned, no action will be taken.
   func userDidDoubleClickOnCell(row rowIndex: Int, column columnIndex: Int) -> Bool
 
+  // The user double-clicked on the cell with the given row & coumn indexes.
+  // If true is returned, an in-line editor will be opened for the first editable cell in that row.
+  // If false is returned, no action will be taken.
   func userDidPressEnterOnRow(_ rowIndex: Int) -> Bool
 
   /*
@@ -24,7 +29,8 @@ protocol EditableTableViewDelegate {
    Each of these 4 actions are built into AppKit and will be called in various places: possibly the Edit menu, key equivalents, or toolbar items.
    No assuptions should be made about calling context - just use the state of the table to see what (if anything) should be copied/etc.
 
-   The Edit menu (et all) look for @objc functions named `cut`, `copy`, `paste`, and `delete` and bear the signatures below.
+   The Edit menu (et al.) look for @objc functions named `cut`, `copy`, `paste`, and `delete` with signatures like:
+   `@objc func copy(_ sender: AnyObject?)`
    It goes down the responder chain looking for them, so ideally they should be defined in the first responder's class.
    This means NSTableView or its subclasses, NOT its delegates! Each action will be called only if it exists and passes validation.
 
@@ -32,8 +38,8 @@ protocol EditableTableViewDelegate {
    The responder chain is checked to to see if `validateUserInterfaceItem()` is enabled.
    Each action is disabled by default, and only enabled if this method is present, and returns `true` in response to the associated action.
 
-   This class adds stubs for all the needed functions to `NSTableViewDelegate`, which will be called by `EditableTableView` when appropriate.
-   They do not need to be @objc functions.
+   `EditableTableView` adds stubs for all the needed functions, and calls the associated action handlers in `NSTableViewDelegate`
+   when appropriate (see `doEditMenu*` below). The delegate functions do not need to be annotated with @objc.
    */
 
   // Callbacks for Edit menu item enablement. Delegates should override these if they want to support the standard operations.
@@ -60,15 +66,24 @@ protocol EditableTableViewDelegate {
 // Adds null defaults for all protocol methods
 extension EditableTableViewDelegate {
   func editDidEndWithNewText(newValue: String, row rowIndex: Int, column columnIndex: Int) -> Bool {
-    // This method should be overriden, so this message should not be seen
+    // If in-line editing is enabled, then this method should be overriden, so this message should never be seen.
     Logger.log("EditableTableViewDelegate.editDidEndWithNewText(): null default method was called!", level: .warning)
     return false
   }
 
   func userDidDoubleClickOnCell(row rowIndex: Int, column columnIndex: Int) -> Bool {
+    // If in-line editing is enabled, then this method should be overriden, so this message should never be seen.
     Logger.log("EditableTableViewDelegate.userDidDoubleClickOnCell(): null default method was called!", level: .warning)
     return false
   }
+
+  func userDidPressEnterOnRow(_ rowIndex: Int) -> Bool {
+    // If in-line editing is enabled, then this method should be overriden, so this message should never be seen.
+    Logger.log("EditableTableViewDelegate.userDidPressEnterOnRow(): null default method was called!", level: .warning)
+    return false
+  }
+
+  // Edit > Cut, Copy, Paste, Delete are disabled by default.
 
   func isCutEnabled() -> Bool {
     false
