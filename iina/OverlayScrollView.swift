8c0e3e6b880ddc4c6b62824e5b7de670e9ff5618
Matt Svoboda <matt.svoboda@gmail.com>
2022-10-25 23:23:42 -0700

    Fix minor conflicting constraints

---
+++ iina/OverlayScrollView.swift
@@ -0,0 +1,27 @@
+//
+//  OverlayScrollView.swift
+//  iina
+//
+//  Created by Matt Svoboda on 10/25/22.
+//  Copyright © 2022 lhc. All rights reserved.
+//
+ 
+import Foundation
+
+class OverlayScrollview: NSScrollView {
+//  override var scrollerStyle: NSScroller.Style {
+//    get {
+//      return .overlay
+//    }
+//    set {
+//
+//    }
+//
+//  }
+}
+
+class MyScroller: NSScroller {
+  func drawBackground(inClipRect: NSRect) {
+    
+  }
+}
