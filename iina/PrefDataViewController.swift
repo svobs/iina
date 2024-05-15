d014b0d3a561b85ddc231253ee064f3832422d99
Matt Svoboda <matt.svoboda@gmail.com>
2024-05-14 19:52:06 -0700

    Add label displaying total history count to Saved Data view

--- iina/PrefDataViewController.swift
+++ iina/PrefDataViewController.swift
@@ -31,37 +31,65 @@ class PrefDataViewController: PreferenceViewController, PreferenceWindowEmbeddab
   @IBOutlet var historyView: NSView!
   @IBOutlet var watchLaterView: NSView!
 
+  @IBOutlet var historyCountView: NSTextField!
+
   @IBOutlet var watchLaterCountView: NSTextField!
   @IBOutlet var watchLaterOptionsView: NSTextField!
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    NotificationCenter.default.addObserver(self, selector: #selector(self.reloadWatchLaterOptions(_:)),
+    NotificationCenter.default.addObserver(self, selector: #selector(self.reloadWatchLaterViews(_:)),
                                            name: .watchLaterOptionsDidChange, object: nil)
+
+    NotificationCenter.default.addObserver(self, selector: #selector(self.reloadHistoryCount(_:)),
+                                           name: .iinaHistoryUpdated, object: nil)
+
   }
 
   override func viewWillAppear() {
     super.viewWillAppear()
 
-    reloadWatchLaterOptions(nil)
+    reloadWatchLaterViews(nil)
+    reloadHistoryCount(nil)
+  }
 
+  // MARK: - IBActions
+
+  @objc func reloadHistoryCount(_ sender: AnyObject?) {
+    let historyCount = HistoryController.shared.history.count
+    DispatchQueue.main.async { [self] in
+      Logger.log("Refreshing History count: \(historyCount)", level: .verbose)
+      historyCountView.stringValue = "Playback history exists for \(historyCount) media."
+    }
+  }
+
+  @objc func reloadWatchLaterViews(_ sender: AnyObject?) {
+    guard !view.isHidden else { return }
+    Logger.log("Refreshing Watch Later views", level: .verbose)
+
+    watchLaterOptionsView.stringValue = MPVController.watchLaterOptions.replacingOccurrences(of: ",", with: ", ")
+
+    refreshWatchLaterCount()
+  }
+
+  private func refreshWatchLaterCount() {
     var watchLaterCount = 0
     let searchOptions: FileManager.DirectoryEnumerationOptions = [.skipsHiddenFiles, .skipsPackageDescendants, .skipsSubdirectoryDescendants]
     if let files = try? FileManager.default.contentsOfDirectory(at: Utility.watchLaterURL, includingPropertiesForKeys: nil, options: searchOptions) {
       watchLaterCount = files.count
     }
+    Logger.log("Found Watch Later data for \(watchLaterCount) media files", level: .verbose)
     watchLaterCountView.stringValue = "Found Watch Later data for \(watchLaterCount) media files."
   }
-
-  @objc func reloadWatchLaterOptions(_ sender: AnyObject?) {
-    watchLaterOptionsView.stringValue = MPVController.watchLaterOptions.replacingOccurrences(of: ",", with: ", ")
-  }
-
-  // MARK: - IBAction
 
   @IBAction func showWatchLaterDirAction(_ sender: AnyObject) {
     NSWorkspace.shared.open(Utility.watchLaterURL)
   }
 
+  @IBAction func rememberRecentChanged(_ sender: NSButton) {
+    if sender.state == .off {
+      AppDelegate.shared.clearRecentDocuments(self)
+    }
+  }
 }
--- iina/PrefGeneralViewController.swift
+++ iina/PrefGeneralViewController.swift
@@ -45,10 +45,5 @@ class PrefGeneralViewController: PreferenceViewController, PreferenceWindowEmbed
     }
   }
 
-  @IBAction func rememberRecentChanged(_ sender: NSButton) {
-    if sender.state == .off {
-      AppDelegate.shared.clearRecentDocuments(self)
-    }
-  }
 }
 
