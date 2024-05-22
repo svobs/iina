9da2910b8e7cdedb0288902b924007fcb020b71a
Matt Svoboda <matt.svoboda@gmail.com>
2024-05-22 15:19:22 -0700

    Tweak open windows algo

--- iina/AppDelegate.swift
+++ iina/AppDelegate.swift
@@ -479,13 +479,16 @@ class AppDelegate: NSObject, NSApplicationDelegate, SPUUpdaterDelegate {
       guard !isTerminating else {
         return
       }
+      // This notification can also happen after windowDidClose notification,
+      // so make sure this a window which is recognized.
       if Preference.UIState.windowsHidden.remove(activeWindowName) != nil {
         Logger.log("Hidden window become main; adding to open windows list: \(activeWindowName.quoted)")
         Preference.UIState.windowsOpen.insert(activeWindowName)
       } else if Preference.UIState.windowsMinimized.remove(activeWindowName) != nil {
         Logger.log("Minimized window become main; adding to open windows list: \(activeWindowName.quoted)")
         Preference.UIState.windowsOpen.insert(activeWindowName)
-      } else {
+      } else if !Preference.UIState.windowsOpen.contains(activeWindowName) {
+        // We still want to save if window is open, because window order could have changed.
         Logger.log("Unrecognized window became main, ignoring: \(activeWindowName.quoted)")
         return
       }
