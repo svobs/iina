9e3cc4b5eba74b41a9ea498bede7b62816e69a94
Matt Svoboda <2213815+svobs@users.noreply.github.com>
2025-01-10 20:07:57 -0800

    Cleanup: move SliderScrollWheelDelegate into ScrollableSlider.swift

--- iina/ScrollableSlider.swift
+++ iina/ScrollableSlider.swift
@@ -6,6 +6,82 @@
 
 import Foundation
 
+/// A subclass of `VirtualScrollWheel` which handles all scroll wheel events for a given `ScrollableSlider`.
+class SliderScrollWheelDelegate: VirtualScrollWheel {
+  let slider: ScrollableSlider
+
+  init(slider: ScrollableSlider, _ log: Logger.Subsystem) {
+    self.slider = slider
+    super.init()
+    self.log = log
+  }
+
+  /// Called zero-to-many times after `scrollSessionWillBegin` & before `scrollSessionDidEnd`.
+  /// Subclasses should override.
+  override func scrollDidUpdate(_ session: ScrollSession) {
+    let valueDelta: CGFloat = session.consumePendingEvents(for: slider)
+    let newValue = (slider.doubleValue + Double(valueDelta)).clamped(to: slider.range)
+    // Prevent very tiny gestures from activating the scroll action.
+    // Some actions (e.g. volume) should show an OSD even if slider.doubleValue doesn't change,
+    // like when they are at max, so that user receives feedback. A sizeable valueDelta.magnitude
+    // can indicate a user intent in this case.
+    guard newValue != slider.doubleValue || valueDelta.magnitude > 1.0 else { return }
+    slider.doubleValue = newValue
+    slider.sendAction(slider.action, to: slider.target)
+  }
+
+  override func scrollSessionDidEnd(_ session: ScrollSession) {
+#if DEBUG
+    if DebugConfig.enableScrollWheelDebug, let player = slider.associatedPlayer {
+      let timeTotal = session.startTime.timeIntervalToNow
+      let timeUser: TimeInterval
+      let timeMsg: String
+      if let momTime = session.momentumStartTime?.timeIntervalToNow {
+        timeUser = timeTotal - momTime
+        timeMsg = "\(timeUser.string2FractionDigits)s user  +  \(momTime.string2FractionDigits)s inertia  =  \(timeTotal.string2FractionDigits)s"
+      } else {
+        timeUser = timeTotal
+        timeMsg = "\(timeTotal.string2FractionDigits)s"
+      }
+      let actionsPerSec = CGFloat(session.actionCount) / timeTotal
+      let actionRatio = CGFloat(session.actionCount) / CGFloat(session.totalEventCount)
+      let deltaPerUserSec = session.rawDeltaTotal / timeUser
+      let accelerationPerUserSec = deltaPerUserSec / timeUser
+      let valueChange: String
+      if let valueAtStart = session.valueAtStart {
+        let valueAtEnd = slider.doubleValue
+        valueChange = "  ⏐  \((valueAtEnd - valueAtStart).stringMaxFrac2) ⱽᴬᴸᵁᴱ"
+      } else {
+        valueChange = ""
+      }
+      let modelValueChange: String
+      if let modelValueAtStart = session.modelValueAtStart, let modelValueAtEnd = session.modelValueAtEnd {
+        let change = modelValueAtEnd - modelValueAtStart
+        modelValueChange = "  ⏐ Δt:  \(change.stringMaxFrac4)s"
+      } else {
+        modelValueChange = ""
+      }
+      let msg = "ScrollWheel Δ: ⏐  \(session.rawDeltaTotal.string2FractionDigits) ᴿᴬᵂ\(valueChange)\(modelValueChange)"
+      let detail = [
+        "Time:       \t\(timeMsg)",
+        "Events:     \t\(session.totalEventCount)",
+        "Actions:    \t\(session.actionCount)    (\(actionsPerSec.stringMaxFrac2)/s, \(actionRatio.stringMaxFrac2)/event)",
+        "Sensitivity: \(session.sensitivity.stringMaxFrac2)",
+        "Avg.Speed:  \(deltaPerUserSec.stringMaxFrac2)/s",
+        "Accel:      \t\(accelerationPerUserSec.magnitude.stringMaxFrac2)/s²",
+      ].joined(separator: "\n")
+      player.sendOSD(.debug(msg, detail))
+    }
+#endif
+  }
+
+  func configure(_ slider: ScrollableSlider, _ log: Logger.Subsystem) {
+    self.log = log
+    slider.scrollWheelDelegate = self
+  }
+}
+
+
 /// Adds scroll wheel support to `NSSlider`. Must set its `scrollWheelDelegate` property after init.
 class ScrollableSlider: NSSlider {
   var scrollWheelDelegate: SliderScrollWheelDelegate?
--- iina/VirtualScrollWheel.swift
+++ iina/VirtualScrollWheel.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+// MARK: - Support classes
+
 /// This is used for 2 different purposes, each using a different subset of states, which is a little sloppy.
 /// But at least it will stay within this file. (See `state` variable vs `mapPhasesToScrollState()`)
 fileprivate enum ScrollState {
@@ -29,6 +31,7 @@ fileprivate enum ScrollState {
   case momentumScrollJustEnded
 }
 
+/// Support class for `VirtualScrollWheel` which exists only for the duration of a single continuous scroll.
 class ScrollSession {
   /// Slider `doubleValue` at scroll start
   var valueAtStart: Double? = nil
@@ -77,82 +80,6 @@ class ScrollSession {
       return rawDeltaSum * sensitivity
     }
   }
-}
-
-/// A subclass of `VirtualScrollWheel` which handles all scroll wheel events for a given `ScrollableSlider`.
-class SliderScrollWheelDelegate: VirtualScrollWheel {
-  let slider: ScrollableSlider
-
-  init(slider: ScrollableSlider, _ log: Logger.Subsystem) {
-    self.slider = slider
-    super.init()
-    self.log = log
-  }
-
-  /// Called zero-to-many times after `scrollSessionWillBegin` & before `scrollSessionDidEnd`.
-  /// Subclasses should override.
-  override func scrollDidUpdate(_ session: ScrollSession) {
-    let valueDelta: CGFloat = session.consumePendingEvents(for: slider)
-    let newValue = (slider.doubleValue + Double(valueDelta)).clamped(to: slider.range)
-    // Prevent very tiny gestures from activating the scroll action.
-    // Some actions (e.g. volume) should show an OSD even if slider.doubleValue doesn't change,
-    // like when they are at max, so that user receives feedback. A sizeable valueDelta.magnitude
-    // can indicate a user intent in this case.
-    guard newValue != slider.doubleValue || valueDelta.magnitude > 1.0 else { return }
-    slider.doubleValue = newValue
-    slider.sendAction(slider.action, to: slider.target)
-  }
-
-  override func scrollSessionDidEnd(_ session: ScrollSession) {
-#if DEBUG
-    if DebugConfig.enableScrollWheelDebug, let player = slider.associatedPlayer {
-      let timeTotal = session.startTime.timeIntervalToNow
-      let timeUser: TimeInterval
-      let timeMsg: String
-      if let momTime = session.momentumStartTime?.timeIntervalToNow {
-        timeUser = timeTotal - momTime
-        timeMsg = "\(timeUser.string2FractionDigits)s user  +  \(momTime.string2FractionDigits)s inertia  =  \(timeTotal.string2FractionDigits)s"
-      } else {
-        timeUser = timeTotal
-        timeMsg = "\(timeTotal.string2FractionDigits)s"
-      }
-      let actionsPerSec = CGFloat(session.actionCount) / timeTotal
-      let actionRatio = CGFloat(session.actionCount) / CGFloat(session.totalEventCount)
-      let deltaPerUserSec = session.rawDeltaTotal / timeUser
-      let accelerationPerUserSec = deltaPerUserSec / timeUser
-      let valueChange: String
-      if let valueAtStart = session.valueAtStart {
-        let valueAtEnd = slider.doubleValue
-        valueChange = "  ⏐  \((valueAtEnd - valueAtStart).stringMaxFrac2) ⱽᴬᴸᵁᴱ"
-      } else {
-        valueChange = ""
-      }
-      let modelValueChange: String
-      if let modelValueAtStart = session.modelValueAtStart, let modelValueAtEnd = session.modelValueAtEnd {
-        let change = modelValueAtEnd - modelValueAtStart
-        modelValueChange = "  ⏐ Δt:  \(change.stringMaxFrac4)s"
-      } else {
-        modelValueChange = ""
-      }
-      let msg = "ScrollWheel Δ: ⏐  \(session.rawDeltaTotal.string2FractionDigits) ᴿᴬᵂ\(valueChange)\(modelValueChange)"
-      let detail = [
-        "Time:       \t\(timeMsg)",
-        "Events:     \t\(session.totalEventCount)",
-        "Actions:    \t\(session.actionCount)    (\(actionsPerSec.stringMaxFrac2)/s, \(actionRatio.stringMaxFrac2)/event)",
-        "Sensitivity: \(session.sensitivity.stringMaxFrac2)",
-        "Avg.Speed:  \(deltaPerUserSec.stringMaxFrac2)/s",
-        "Accel:      \t\(accelerationPerUserSec.magnitude.stringMaxFrac2)/s²",
-      ].joined(separator: "\n")
-      player.sendOSD(.debug(msg, detail))
-    }
-#endif
-  }
-
-  func configure(_ slider: ScrollableSlider, _ log: Logger.Subsystem) {
-    self.log = log
-    slider.scrollWheelDelegate = self
-  }
-
 }
 
 /// This class provides a wrapper API over Apple's APIs to simplify scroll wheel handling. This is an abstract base class which is
