svobs/iina

commit c8bed48505498741de5c7ab4c0adad31ed196109

Fix more plugin sidebar bugs

iina/QuickSettingViewController.swift
@@ -568,6 +568,8 @@ class QuickSettingViewController: NSViewController, NSTableViewDataSource, NSTab
 
   private func switchToTab(_ tab: Sidebar.Tab) {
     guard isViewLoaded else { return }
+    assert(player.windowController.isShowing(sidebarTabGroup: .settings),
+           "switchToTab should not be called when settings TabGroup is not shown")
     guard currentTab != tab else { return }
     guard tab.group == .settings else {
       player.log.error("QuickSettingsViewController: cannot switch to tab: \(tab)")

iina/JavascriptAPICore.swift
@@ -237,7 +237,7 @@ fileprivate class WindowAPI: JavascriptAPI, CoreSubAPIExportable {
     case "visible":
       return window.window!.occlusionState == .visible
     case "sidebar":
-      return window.isShowingSettingsSidebar() ? window.quickSettingView.currentTab.name : NSNull()
+      return window.isShowing(sidebarTabGroup: .settings) ? window.quickSettingView.currentTab.name : NSNull()
     case "screens":
       let current = window.window!.screen!
       let main = NSScreen.main

iina/PWinGeometry.swift
@@ -1014,7 +1014,7 @@ struct PWinGeometry: Equatable, CustomStringConvertible {
     let ΔOutsideWidth = geo.outsideBars.totalWidth - outsideBars.totalWidth
     let ΔOutsideHeight = geo.outsideBars.totalHeight - outsideBars.totalHeight
 
-    log.debug("[ResizeBars] W={Δ:\(ΔOutsideWidth.logStr) fill:\(fillWidthOfScreen.yn)}, H={Δ:\(ΔOutsideHeight.logStr) fill:\(fillHeightOfScreen.yn)) moveToKeepInScreen:\(geo.screenFit.shouldMoveWindowToKeepInContainer.yesno)")
+    log.verbose("[ResizeBars] W={Δ:\(ΔOutsideWidth.logStr) fill:\(fillWidthOfScreen.yn)}, H={Δ:\(ΔOutsideHeight.logStr) fill:\(fillHeightOfScreen.yn)) moveToKeepInScreen:\(geo.screenFit.shouldMoveWindowToKeepInContainer.yesno)")
 
     let newViewportSize: NSSize
     // If window already fills screen width, do not shrink window width when collapsing outside sidebars.

iina/PWin_Sidebar.swift
@@ -246,10 +246,9 @@ struct Sidebar {
 extension PlayerWindowController {
   // MARK: - Show/Hide functions
 
-  // For JavascriptAPICore:
-  func isShowingSettingsSidebar() -> Bool {
+  func isShowing(sidebarTabGroup tabGroup: Sidebar.TabGroup) -> Bool {
     let layout = currentLayout
-    return layout.leadingSidebar.visibleTabGroup == .settings || layout.trailingSidebar.visibleTabGroup == .settings
+    return layout.leadingSidebar.visibleTabGroup == tabGroup || layout.trailingSidebar.visibleTabGroup == tabGroup
   }
 
   func isShowing(sidebarTab tab: Sidebar.Tab) -> Bool {
@@ -271,7 +270,7 @@ extension PlayerWindowController {
       guard currentLayout.canShowSidebars else { return }
       let sidebar = currentLayout.sidebar(withID: sidebarID)
       let isCurrentlyVisible = sidebar.isVisible
-      log.verbose{"Toggling visibility of sidebar \(sidebarID): isVisible: \(isCurrentlyVisible.yn) → \((!isCurrentlyVisible).yn))"}
+      log.verbose{"Toggling visibility of sidebar \(sidebarID): \(isCurrentlyVisible.yn) → \((!isCurrentlyVisible).yn)"}
       // Do nothing if sidebar has no configured tabs
       guard let tab = sidebar.defaultTabToShow else { return }
 
@@ -703,8 +702,12 @@ extension PlayerWindowController {
   }
 
   // MARK: - Changing tabs
-  
+
+  /// Assuming the correct sidebar is already showing, & it is showing the correct tab group, switches to the given tab in the same tab group.
   func switchToTabInTabGroup(tab: Sidebar.Tab) {
+    assert((getConfiguredSidebar(forTabGroup: tab.group)!.visibleTabGroup == tab.group),
+           "switchToTabInTabGroup: expected TabGroup \(tab.group) to be visible")
+
     // Make it the active tab in its parent tab group (can do this whether or not it's shown):
     switch tab.group {
     case .playlist:

iina/PlayerCore.swift
@@ -343,6 +343,8 @@ class PlayerCore: NSObject {
   func clearPlugins() {
     pluginMap.removeAll()
     plugins.removeAll()
+
+    windowController.pluginView.updatePluginTabs()
   }
 
   func loadPlugins() {
@@ -353,6 +355,8 @@ class PlayerCore: NSObject {
       pluginMap[plugin.identifier] = instance
       return instance
     }
+
+    windowController.pluginView.updatePluginTabs()
   }
 
   func reloadPlugin(_ plugin: JavascriptPlugin, forced: Bool = false) {
@@ -562,12 +566,6 @@ class PlayerCore: NSObject {
     /// This will create & add the `GLVideoLayer` if it was not already init:
     videoView.wantsLayer = true
     loadPlugins()
-    windowController.pluginView.updatePluginTabs()
-    if case .restoring(let priorState) = windowController.sessionState,
-       let selectedPluginTabID = priorState.layoutSpec?.moreSidebarState.selectedPluginTabID {
-      // Restore selected plugin tab
-      windowController.pluginView.pleaseSwitchToTab(selectedPluginTabID)
-    }
     if isAudioOnly {
       log.debug("Player is audio only. Will not init video")
     } else {

iina/PlaylistViewController.swift
@@ -310,15 +310,19 @@ class PlaylistViewController: NSViewController, NSTableViewDataSource, NSTableVi
       player.log.error("PlaylistViewController: cannot switch to tab: \(tab)")
       return
     }
+    assert(player.windowController.isShowing(sidebarTabGroup: .playlist),
+           "switchToTab should not be called when playlist TabGroup is not shown")
+    let buttonTag: Int
     switch tab {
     case .playlist:
       refreshNowPlayingIndex()
-      tabView.selectTabViewItem(at: 0)
+      buttonTag = 0
     case .chapters:
-      tabView.selectTabViewItem(at: 1)
+      buttonTag = 1
     default:
       Logger.fatal("PlaylistViewController: invalid tab requested for switching: \(tab)")
     }
+    tabView.selectTabViewItem(at: buttonTag)
 
     currentTab = tab
     updateTabButtons()

iina/PluginViewController.swift
@@ -89,6 +89,11 @@ class PluginViewController: NSViewController, SidebarTabGroupViewController {
 
   func updatePluginTabs() {
     guard isViewLoaded else { return }
+
+    guard player.windowController.isShowing(sidebarTabGroup: .plugins) else {
+      player.log.verbose("Skipping update of Plugins sidebar; it is not visible")
+      return
+    }
     pluginTabsStackView.arrangedSubviews.forEach {
       pluginTabsStackView.removeArrangedSubview($0)
     }
@@ -131,6 +136,8 @@ class PluginViewController: NSViewController, SidebarTabGroupViewController {
 
   private func switchToTab(_ tabID: String) {
     guard isViewLoaded else { return }
+    assert(player.windowController.isShowing(sidebarTabGroup: .plugins),
+           "switchToTab should not be called when plugins TabGroup is not shown")
     currentPluginID = tabID
     updateTabActiveStatus()
 

commit 7b3249bafff23ba2c9c47ca433dd4744f0c8af31

Removed key window requirement from window focus test. Fix regression to tab height introduced in c8bed48505498741de5c7ab4c0adad31ed196109. Set OSCToolbarButton width to required, so that it is no longer ambiguous

iina/OSCToolbarButton.swift
@@ -52,7 +52,7 @@ class OSCToolbarButton: SymButton {
       widthConstraint.animateToConstant(iconSize)
     } else {
       let constraint = widthAnchor.constraint(equalToConstant: iconSize)
-      constraint.priority = .defaultHigh
+      constraint.priority = .required
       constraint.isActive = true
       self.widthConstraint = constraint
     }

iina/PWin_Input.swift
@@ -550,7 +550,7 @@ extension PlayerWindowController {
   }
 
   func isMouseActuallyInside(view: NSView) -> Bool {
-    return window!.isKeyWindow && isPoint(mouseLocationInWindow, inAnyOf: [view])
+    return isPoint(mouseLocationInWindow, inAnyOf: [view])
   }
 
   // assumes mouse is in window

iina/QuickSettingViewController.swift
@@ -229,6 +229,8 @@ class QuickSettingViewController: NSViewController, NSTableViewDataSource, NSTab
     // colors
     withAllTableViews { tableView, _ in tableView.backgroundColor = NSColor.sidebarTableBackground }
 
+    updateVerticalConstraints()
+
     if pendingSwitchRequest == nil {
       updateTabButtons()
     } else {

iina/Slider/VolumeSliderCell.swift
@@ -52,7 +52,7 @@ class VolumeSliderCell: ScrollableSliderCell {
     }
   }
 
-  @objc func refreshVolumeSliderHoverEffect() {
+  func refreshVolumeSliderHoverEffect() {
     guard let wc else { return }
     let priorHoverState = isMouseHoveringOverVolumeSlider
     let newHoverState = wc.isMouseActuallyInside(view: slider)